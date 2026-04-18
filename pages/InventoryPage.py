6d434d7964e5d814c5c15c4df63fe17b3a811861
parovozUA <carbitnis@gmail.com>
2026-04-19 01:47:30 +0200

    refactor(inventory-page)
    redutant code removed, added locators

--- pages/InventoryPage.py
+++ pages/InventoryPage.py
@@ -1,31 +1,48 @@
-from playwright.sync_api import expect
+from enum import Enum
 
 from pages.BasePage import BasePage
 
+class SortValue(str, Enum):
+    PRICE_ASC = "lohi"
+    PRICE_DESC = "hilo"
+    NAME_ASC = "az"
+    NAME_DESC = "za"
 
 class InventoryPage(BasePage):
     URL = "https://www.saucedemo.com/inventory.html"
+    ITEMS_COUNT = 6 # According to current application behavior, inventory contains 6 items
 
     def __init__(self, page):
         super().__init__(page)
-        self.inventory_list = page.locator(".inventory_list")
-        self.title = page.locator(".title")
+        self.inventory_list = page.get_by_test_id("inventory-list")
+        self.items = self.inventory_list.get_by_test_id("inventory-item")
 
-        # новый элемент
-        self.add_to_cart_button = page.locator("[data-test='add-to-cart-sauce-labs-backpack']")
-        self.cart_icon = page.locator(".shopping_cart_link")
-        self.cart_badge = page.locator(".shopping_cart_badge")
+        # price and name
+        self.item_name_texts = self.items.get_by_test_id("inventory-item-name")
+        self.item_price_texts = self.items.get_by_test_id("inventory-item-price")
+
+        # backpack buttons
+        self.add_to_cart_backpack_button = page.get_by_test_id("add-to-cart-sauce-labs-backpack")
+        self.remove_from_cart_backpack_button = page.get_by_test_id("remove-sauce-labs-backpack")
+
+        # sorting
+        self.sort_dropdown = page.get_by_test_id("product-sort-container")
+
+        self.cart_icon = page.get_by_test_id("shopping-cart-link")
+        self.cart_badge = page.get_by_test_id("shopping-cart-badge")
 
     def open(self): self.page.goto(self.URL)
 
     def get_url(self):
         return self.page.url
 
-    def assert_loaded(self):
-        return expect(self.inventory_list).to_be_visible()
 
+    def sort_items(self, sort_by: SortValue):
+        self.sort_dropdown.select_option(value=sort_by)
+
+    # ------------- Cart actions -------------
     def add_item_to_cart(self):
-        self.add_to_cart_button.click()
+        self.add_to_cart_backpack_button.click()
 
     def open_cart(self):
         self.cart_icon.click()
