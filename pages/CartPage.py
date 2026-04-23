9d1e0e3069a903806a7d1f596377eddf7cbf3759
parovozUA <carbitnis@gmail.com>
2026-04-23 18:35:49 +0200

    refactor(cart): replaced locator with test id

--- pages/CartPage.py
+++ pages/CartPage.py
@@ -5,8 +5,8 @@ class CartPage(BasePage):
 
     def __init__(self, page):
         super().__init__(page)
-        self.cart_items = page.locator(".cart_item")
-        self.checkout_button = page.locator("[data-test='checkout']")
+        self.cart_items = page.get_by_test_id("inventory-item")
+        self.checkout_button = page.get_by_test_id("checkout")
 
     def is_item_present(self):
         return self.cart_items.count() > 0
