7c761cb5a83a61eb5b8a2ed1939210c76391aefa
parovozUA <carbitnis@gmail.com>
2026-04-23 22:34:12 +0200

    refactor(login): replace hardcoded credentials with Users data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 
+from data.Users import Users
 from pages.LoginPage import LoginPage
 
 @pytest.fixture
 def logged_in_page(page):
     login_page = LoginPage(page)
     login_page.open()
-    login_page.login("standard_user", "secret_sauce")
+    login_page.login(Users.VALID)
     return page
