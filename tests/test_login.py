f772b4c3c8504cf1febd411fe3ef4768ea8e9756
parovozUA <carbitnis@gmail.com>
2026-04-24 00:13:16 +0200

    test(login): verify valid and invalid login credential paths

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,15 +8,17 @@ from pages.LoginPage import LoginPage
 
 
 @pytest.mark.parametrize(
-    "user",
+    "user, should_pass",
     [
-        Users.WRONG_USERNAME,
-        Users.WRONG_PASSWORD,
-        Users.EMPTY_USERNAME,
-        Users.EMPTY_PASSWORD,
-        Users.LOCKED,
+        (Users.VALID, True),
+        (Users.WRONG_USERNAME, False),
+        (Users.WRONG_PASSWORD, False),
+        (Users.EMPTY_USERNAME, False),
+        (Users.EMPTY_PASSWORD, False),
+        (Users.LOCKED, False),
     ],
     ids=[
+        "valid user",
         "wrong username",
         "wrong password",
         "empty username",
@@ -24,18 +26,13 @@ from pages.LoginPage import LoginPage
         "locked user",
     ]
 )
-def test_login_invalid(page, user):
+def test_login(page, user, should_pass):
     login_page = LoginPage(page)
 
     login_page.open()
     login_page.login(user)
 
-    expect(login_page.error_message).to_be_visible()
-
-def test_login_success(page):
-    login_page = LoginPage(page)
-
-    login_page.open()
-    login_page.login(Users.VALID)
-
-    expect(page).to_have_url(re.compile(r".*inventory.*"))
+    if should_pass:
+        expect(page).to_have_url(re.compile(r".*inventory.*"))
+    else:
+        expect(login_page.error_message).to_be_visible()
